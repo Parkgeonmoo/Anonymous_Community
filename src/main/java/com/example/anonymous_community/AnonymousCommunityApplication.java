d512022fbc2dd9b44fd79ee6b9115d24e7e3ff3a
parkgeonwoo <pgw111111@naver.com>
2023-09-14 12:28:04 +0900

    Chore: 프로젝트 환경 셋팅
    - SpringBoot 2.5.6 버전 적용
    - JAVA 11 버전 적용

---
+++ src/main/java/com/example/anonymous_community/AnonymousCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.anonymous_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anonymous_community/AnonymousCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anonymous_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
