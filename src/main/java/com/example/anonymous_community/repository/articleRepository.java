0903fc9a2fbf4cf678989b3817d3a1ba346ae180
parkgeonwoo <pgw111111@naver.com>
2023-09-14 17:14:03 +0900

    Feat:JPA 사용을 위한 인터페이스 구현 -글 Entity에 대한 객체를 Spring-data-jpa를 통해 통신하기 위해 구현

---
+++ src/main/java/com/example/anonymous_community/repository/articleRepository.java
@@ -0,0 +1,9 @@
+package com.example.anonymous_community.repository;
+
+import com.example.anonymous_community.entity.articleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface articleRepository extends JpaRepository<articleEntity,Integer> {
+}
