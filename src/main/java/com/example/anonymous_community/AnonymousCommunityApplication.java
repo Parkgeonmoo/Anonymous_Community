4c03747943661e6c7b0c7ea1283173261de0511c
parkgeonwoo <pgw111111@naver.com>
2023-09-15 18:09:53 +0900

    Test: Slack 알림을 위한 테스트 어노테이션 및 코드 작성 - Slack 알람이 프로젝트에서 잘 발생하는지 테스트 어노테이션과 코드 작성

--- src/main/java/com/example/anonymous_community/AnonymousCommunityApplication.java
+++ src/main/java/com/example/anonymous_community/AnonymousCommunityApplication.java
@@ -1,13 +1,19 @@
 package com.example.anonymous_community;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Slf4j
 @SpringBootApplication
 public class AnonymousCommunityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnonymousCommunityApplication.class, args);
-    }
 
+            //log.error("에러 테스트");
+
+    }
 }
