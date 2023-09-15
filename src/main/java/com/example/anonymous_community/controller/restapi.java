b0be5afd2b84f4f5d6d78368f15b340f4f1dc85d
parkgeonwoo <pgw111111@naver.com>
2023-09-15 18:11:34 +0900

    Refactor:메소드 반환 타입 수정 - Controller에서 받아오는 값들을 ResponseEntity 객체를 통한 JSON 값으로 받도록 수정하여 구현

--- src/main/java/com/example/anonymous_community/controller/restapi.java
+++ src/main/java/com/example/anonymous_community/controller/restapi.java
@@ -1,12 +1,63 @@
 package com.example.anonymous_community.controller;
 
 import com.example.anonymous_community.dto.article;
+import com.example.anonymous_community.entity.articleEntity;
+import com.example.anonymous_community.service.articleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+@RestController
+@RequestMapping("/api")
+public class restapi {
+    articleService articleService = null;
 
-public interface restapi {
-    public Boolean postArticel(String title,String nickname,String contents,String password);
-    public article getArticle(int articleIndex);
-    public void putArticle(int articelsIndex,String title,String nickname,String contents,String password,String newpassword);
-    public void deleteArticle(int articleIndex,String password);
+    public restapi(articleService articleservice) {
+        articleService = articleservice;
+    }
+
+    @PostMapping("/article")
+    public ResponseEntity postArticel(@RequestBody article inputArticle){
+
+        article result = articleService.postArticleService(inputArticle);
+
+        if (result != null) {
+            return ResponseEntity.ok(result);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
+    @GetMapping("/article")
+    public ResponseEntity getArticle(int articleindex){
+        article result = articleService.getArticleService(articleindex);
+
+        if (result != null) {
+            return ResponseEntity.ok(result);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/article")
+    public ResponseEntity putArticle(@RequestBody article inputArticle){
+        article result = articleService.putArticleService(inputArticle);
+        if (result != null) {
+            return ResponseEntity.ok(result);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/article")
+    public ResponseEntity deleteArticle(int articleIndex,String password){
+        article result = articleService.deleteArticleService(articleIndex,password);
+        if (result != null) {
+            return ResponseEntity.ok(result);
+        }else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
