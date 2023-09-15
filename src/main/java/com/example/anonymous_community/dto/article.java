3b87cdc207fff30287780d2a76d6734a10a664e5
parkgeonwoo <pgw111111@naver.com>
2023-09-15 18:13:35 +0900

    Refactor:DTO 객체 수정 - @Data를 통하여 Getter,Setter 및  생성자를 전부 일괄 생성하도록 수정 - created_Time,updated_Time 멤버변수 추가

--- src/main/java/com/example/anonymous_community/dto/article.java
+++ src/main/java/com/example/anonymous_community/dto/article.java
@@ -1,28 +1,23 @@
 package com.example.anonymous_community.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
+@Data
 public class article {
     int articleIndex;
     String title;
     String nickName;
     String contents;
     String password;
-    String newpassword;
+    String created_Time;
+    String updated_Time;
 
     public article() {
 
     }
-    public article(int articleIndex,String title,String nickName,String contents,String password,String newpassword) {
-        this.articleIndex = articleIndex;
-        this.title = title;
-        this.nickName = nickName;
-        this.contents = contents;
-        this.password = password;
-        this.newpassword = newpassword;
-    }
+
 
 }
