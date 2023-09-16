69781c2d6a1fa9bb3cfe832cba5085350f6a80e3
parkgeonwoo <pgw111111@naver.com>
2023-09-16 20:18:28 +0900

    Feat:댓글 dto 객체 구현 - 댓글에 대한 내용을 담기 위한 dto 객체 구현

--- src/main/java/com/example/anonymous_community/dto/comment.java
+++ src/main/java/com/example/anonymous_community/dto/comment.java
@@ -1,4 +1,18 @@
 package com.example.anonymous_community.dto;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+
+@Data
 public class comment {
+
+    private String commentIndex;
+    private String articleIndex;
+    private String nickName;
+    private String contents;
+    private String password;
+    private String created_Time;
+    private String updated_Time;
+
 }
