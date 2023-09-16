637e65c12e4450c6859ee5eb41ea819a73236f47
parkgeonwoo <pgw111111@naver.com>
2023-09-16 21:12:32 +0900

    Refactor:JPA  PK타입 변경 -JPA PK 타입 int -> String 변경

--- src/main/java/com/example/anonymous_community/repository/articleRepository.java
+++ src/main/java/com/example/anonymous_community/repository/articleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface articleRepository extends JpaRepository<articleEntity,Integer> {
+public interface articleRepository extends JpaRepository<articleEntity,String> {
 }
