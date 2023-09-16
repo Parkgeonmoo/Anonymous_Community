6a625734cafb377a5171d224a4472db7cd36272f
parkgeonwoo <pgw111111@naver.com>
2023-09-16 20:39:19 +0900

    no message

--- src/main/java/com/example/anonymous_community/repository/commentRepository.java
+++ src/main/java/com/example/anonymous_community/repository/commentRepository.java
@@ -1,4 +1,8 @@
 package com.example.anonymous_community.repository;
 
-public interface commentRepository {
+import com.example.anonymous_community.entity.articleEntity;
+import com.example.anonymous_community.entity.commentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface commentRepository extends JpaRepository<commentEntity,String> {
 }
