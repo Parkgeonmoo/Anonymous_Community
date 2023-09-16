86a2cb3afb9963495c0837f44f587cbf2f216dd8
parkgeonwoo <pgw111111@naver.com>
2023-09-16 20:17:33 +0900

    Refactor:변수명 일치하지 않는 부분 수정 및 페이지네이션을 위한 dao 부분 추가 구현 - articleEntity객체의 변수명이 article이라 혼동을 줄 수 있어 articleEntity로 전부 변경 - 페이지네이션을 위한 페이지 타입을 받도록 하는 dao 메소드 구현

--- src/main/java/com/example/anonymous_community/dao/articledao.java
+++ src/main/java/com/example/anonymous_community/dao/articledao.java
@@ -3,13 +3,19 @@ package com.example.anonymous_community.dao;
 import com.example.anonymous_community.dto.article;
 import com.example.anonymous_community.entity.articleEntity;
 import com.example.anonymous_community.repository.articleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Repository
+@Slf4j
 public class articledao {
     articleRepository articleRepository;
 
@@ -17,32 +23,39 @@ public class articledao {
         this.articleRepository = repository;
     }
 
-    public articleEntity postArticleEntity(articleEntity article){
-        if (article == null) {
+    public articleEntity postArticleEntity(articleEntity articleEntity){
+        if (articleEntity == null) {
             System.out.println("글쓰기 저장 실패입니다. 전달된 객체가 null입니다.");
             return null;
         }
 
         try {
-            articleRepository.save(article);
+
+            articleEntity = articleRepository.save(articleEntity);
         }catch(Exception e) {
             System.out.println("글쓰기 저장 실패입니다.");
             return null;
         }
 
-        return article;
+        return articleEntity;
     }
-    public articleEntity getArticleEntity(int articleIndex){
+    public articleEntity getArticleEntity(String articleIndex){
         Optional<articleEntity> temp = articleRepository.findById(articleIndex);
-        articleEntity article = null;
+        articleEntity articleEntity = null;
         if (temp.isPresent()) {
-            article = temp.get();
+            articleEntity = temp.get();
         }else{
             throw new NoSuchElementException("해당 인덱스의 글이 존재하지 않습니다.");
         }
-        return article;
+        return articleEntity;
 
     }
+
+    public Page<articleEntity> getArticlesEntity(int page, int limit){
+        Pageable pageable = PageRequest.of(page-1, limit);
+        return articleRepository.findAll(pageable);
+    }
+
     public articleEntity putArticleEntity(articleEntity articleEntity){
         Optional<articleEntity> temp = articleRepository.findById(articleEntity.getArticleIndex());
         articleEntity returnArticleEntity = null;
@@ -53,6 +66,7 @@ public class articledao {
                 existingArticle.setNickname(articleEntity.getNickname());
                 existingArticle.setContents(articleEntity.getContents());
                 existingArticle.setPassword(articleEntity.getPassword());
+                existingArticle.setUpdated_time(articleEntity.getUpdated_time());
 
 
                 returnArticleEntity = articleRepository.save(existingArticle);
@@ -65,7 +79,7 @@ public class articledao {
         return returnArticleEntity;
     }
 
-    public articleEntity deleteArticleEntity(int articleIndex, String password){
+    public articleEntity deleteArticleEntity(String articleIndex, String password){
         Optional<articleEntity> temp = articleRepository.findById(articleIndex);
         articleEntity articleEntity = null;
         if(temp.isPresent()){
