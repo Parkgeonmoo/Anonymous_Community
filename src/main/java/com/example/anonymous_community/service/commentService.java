7057cf258da1e5557dcfebeeecd235888c999bde
parkgeonwoo <pgw111111@naver.com>
2023-09-18 15:32:51 +0900

    Feat:comment GET,PUT,DELTE 로직 구현 -서비스 계층에서 Controller로부터 dto로 데이터를 받고 dao 계층에 Entity로 전달한 후 결과값을  dto로 반환하여 Controller계층에 전달해주도록 구현

--- src/main/java/com/example/anonymous_community/service/commentService.java
+++ src/main/java/com/example/anonymous_community/service/commentService.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import com.example.anonymous_community.dao.commentdao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
 public class commentService {
@@ -54,26 +57,72 @@ public class commentService {
 
     }
 
-    public comment getCommentService(String commentIndex) {
-        comment returnComment = new comment();
-        commentEntity commentEntity = new commentEntity();
+    public List<comment> getCommentService(String articleIndex) {
+        List<comment> returnComments = new ArrayList<>();
+        List<commentEntity> commentEntity = new ArrayList<>();
 
         try {
-            commentEntity = commentdao.getCommentEntity(commentIndex);
+            commentEntity = commentdao.getCommentEntities(articleIndex);
 
-            returnComment.setCommentIndex(commentEntity.getCommentIndex());
-            returnComment.setArticleIndex(commentEntity.getArticleIndex());
-            returnComment.setNickName(commentEntity.getNickName());
-            returnComment.setContents(commentEntity.getContents());
-            returnComment.setPassword(commentEntity.getPassword());
-            returnComment.setCreated_Time(commentEntity.getCreated_Time());
-            returnComment.setUpdated_Time(commentEntity.getUpdated_Time());
-
+            for (commentEntity temp : commentEntity) {
+                comment tempComment = new comment();
+                tempComment.setCommentIndex(temp.getCommentIndex());
+                tempComment.setArticleIndex(temp.getArticleIndex());
+                tempComment.setNickName(temp.getNickName());
+                tempComment.setContents(temp.getContents());
+                tempComment.setPassword(temp.getPassword());
+                tempComment.setCreated_Time(temp.getCreated_Time());
+                tempComment.setUpdated_Time(temp.getUpdated_Time());
+                returnComments.add(tempComment);
+            }
         }catch(Exception e) {
             e.printStackTrace();
             System.out.println("조회하신 댓글이 존재하지 않습니다.");
         }
-        return returnComment;
+        return returnComments;
 
+    }
+
+    public comment putCommentService(comment inputComment) {
+        commentEntity commentEntity = new commentEntity();
+        comment returnComment = new comment();
+        commentEntity.setArticleIndex(inputComment.getArticleIndex());
+        commentEntity.setCommentIndex(inputComment.getCommentIndex());
+        commentEntity.setContents(inputComment.getContents());
+        commentEntity.setPassword(inputComment.getPassword());
+
+        commentEntity = commentdao.putCommentEntity(commentEntity);
+
+        if (commentEntity != null) {
+            returnComment.setArticleIndex(commentEntity.getArticleIndex());
+            returnComment.setCommentIndex(commentEntity.getCommentIndex());
+            returnComment.setNickName(commentEntity.getNickName());
+            returnComment.setContents(commentEntity.getContents());
+            returnComment.setUpdated_Time(commentEntity.getUpdated_Time());
+        }else{
+            System.out.println("수정하고자 하는 댓글이 존재하지 않습니다.");
+        }
+         return returnComment;
+    }
+
+    public comment deleteCommentService(comment inputComment) {
+        commentEntity commentEntity = new commentEntity();
+        comment returnComment = new comment();
+        commentEntity.setArticleIndex(inputComment.getArticleIndex());
+        commentEntity.setCommentIndex(inputComment.getCommentIndex());
+        commentEntity.setPassword(inputComment.getPassword());
+
+        commentEntity = commentdao.deleteEntity(commentEntity);
+
+        if (commentEntity != null) {
+            returnComment.setArticleIndex(commentEntity.getArticleIndex());
+            returnComment.setCommentIndex(commentEntity.getCommentIndex());
+            returnComment.setNickName(commentEntity.getNickName());
+            returnComment.setContents(commentEntity.getContents());
+            returnComment.setUpdated_Time(commentEntity.getUpdated_Time());
+        }else{
+            System.out.println("삭제하고자 하는 댓글이 존재하지 않습니다.");
+        }
+        return returnComment;
     }
 }
