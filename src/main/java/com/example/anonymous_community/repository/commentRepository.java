26467d25434cd84864da1695dfdf0fe43f5d3d4a
parkgeonwoo <pgw111111@naver.com>
2023-09-18 15:31:27 +0900

    Feat:JPA 추상 메소드 추가 -comment를 찾아오는 작업을 할 때 필요한 추상 메소드 추가

--- src/main/java/com/example/anonymous_community/repository/commentRepository.java
+++ src/main/java/com/example/anonymous_community/repository/commentRepository.java
@@ -4,5 +4,9 @@ import com.example.anonymous_community.entity.articleEntity;
 import com.example.anonymous_community.entity.commentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface commentRepository extends JpaRepository<commentEntity,String> {
+    List<commentEntity> findByArticleIndex(String articleIndex);
+    commentEntity findByArticleIndexAndCommentIndex(String articleIndex, String commentIndex);
 }
