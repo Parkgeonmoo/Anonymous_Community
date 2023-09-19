bd677d759858321daae3e858fa5e4b1597c5ac92
parkgeonwoo <pgw111111@naver.com>
2023-09-19 13:58:47 +0900

    Refactor:Entity 클래스 명 변동으로 인한 변경

--- src/main/java/com/example/anonymous_community/repository/ArticleRepository.java
+++ src/main/java/com/example/anonymous_community/repository/ArticleRepository.java
@@ -1,9 +1,9 @@
 package com.example.anonymous_community.repository;
 
-import com.example.anonymous_community.entity.articleEntity;
+import com.example.anonymous_community.entity.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<articleEntity,String> {
+public interface ArticleRepository extends JpaRepository<ArticleEntity,String> {
 }
--- src/main/java/com/example/anonymous_community/repository/CommentRepository.java
+++ src/main/java/com/example/anonymous_community/repository/CommentRepository.java
@@ -1,11 +1,11 @@
 package com.example.anonymous_community.repository;
 
-import com.example.anonymous_community.entity.commentEntity;
+import com.example.anonymous_community.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<commentEntity,String> {
-    List<commentEntity> findByArticleIndex(String articleIndex);
-    commentEntity findByArticleIndexAndCommentIndex(String articleIndex, String commentIndex);
+public interface CommentRepository extends JpaRepository<CommentEntity,String> {
+    List<CommentEntity> findByArticleIndex(String articleIndex);
+    CommentEntity findByArticleIndexAndCommentIndex(String articleIndex, String commentIndex);
 }
