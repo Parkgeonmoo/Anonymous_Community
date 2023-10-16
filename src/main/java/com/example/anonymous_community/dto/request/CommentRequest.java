14b2c8a953e784243c48f04d9f94f43d03dee681
parkgeonwoo <pgw111111@naver.com>
2023-10-16 12:26:20 +0900

    Feat:DTO 생성
    -API 공통 Response dto 생성
    -게시글,댓글 dto 생성

---
+++ src/main/java/com/example/anonymous_community/dto/request/CommentRequest.java
@@ -0,0 +1,31 @@
+package com.example.anonymous_community.dto.request;
+
+import com.example.anonymous_community.entity.CommentEntity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 댓글 파라미터 Dto
+ *
+ * @author parkgeonwoo
+ */
+@Data
+@NoArgsConstructor(force = true)
+public class CommentRequest {
+
+    private String commentIndex;
+    private String articleIndex;
+    private String nickName;
+    private String contents;
+    private String password;
+
+    public CommentRequest(CommentEntity commentEntity) {
+        this.commentIndex = commentEntity.getArticleIndex();
+        this.articleIndex = commentEntity.getArticleIndex();
+        this.nickName = commentEntity.getNickName();
+        this.contents = commentEntity.getContents();
+        this.password = commentEntity.getPassword();
+
+    }
+
+}
